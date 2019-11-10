70b9046cdf1ff7135a1b7d9b347a796e6d2f5f85
alexeiryabov <alexei@guarda.co>
2019-11-11 00:09:23 +0300

    Init deploy scheme

---
+++ pm2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [{
+        name: 'changenow-bot',
+        script: './node_modules/.bin/babel-node',
+        args: 'src/index.js',
+        cwd: __dirname,
+        restart_delay: 3000,
+        autorestart: true,
+        max_restarts: 50,
+    }]
+}
