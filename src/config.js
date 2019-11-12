678ec54a639c50b3f3142298839af1b4b85bdfff
davegahn <anxieter@yandex.ru>
2019-11-12 20:07:27 +0300

    reenter key

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config =
 {
   api_url: 'https://changenow.io/api/v1',
-  api_key: process.env.API_CN_KEY,
+  api_key: '0a6d67c58b40a35f949329157a99e96753e64c06aa252d574fd32aa26631f5c2',
   popularCurrs: {
     btc: 'Bitcoin (BTC)',
     eth: 'Ethereum (ETH)',
