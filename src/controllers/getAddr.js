70111506da74c453c56afa22c71eaad5f168a88d
davegahn <anxieter@yandex.ru>
2019-11-25 18:09:49 +0300

    [64]Added transcactions to DB

--- src/controllers/getAddr.js
+++ src/controllers/getAddr.js
@@ -4,19 +4,22 @@ import Stage from 'telegraf/stage';
 import { getTransactionStatus } from '../api';
 import { getBackKeyboard } from '../keyboards';
 import { config } from '../config';
-import { pause, intervalRequire, breakTransaction, getAmountTotal, startHandler } from '../helpers';
+import { pause, intervalRequire, breakTransaction, getAmountTotal, startHandler, addTransactionToDB } from '../helpers';
 import { messages } from '../messages';
+import UserModel from '../models/User';
 
 const { leave } = Stage;
 const getAddress = new Scene('get_addr');
 
 getAddress.enter(async ctx => {
+  const uId = await ctx.session.userId;
   const payinData = await ctx.session.response;
   const amount = ctx.session.amount;
   const curFrom = ctx.session.curFrom;
   const curTo = ctx.session.curTo;
   const fromTo = `${curFrom}_${curTo}`;
   const amountTotal = await getAmountTotal(amount, fromTo);
+  payinData && addTransactionToDB(payinData.id, uId);
   payinData &&
     (await ctx.replyWithHTML(
       `You’re sending <b>${amount} ${curFrom.toUpperCase()}</b>; you’ll get ~<b>${amountTotal} ${curTo.toUpperCase()}</b>.\nHere is the deposit address for your exchange.\nIn order to start the exchange, use your wallet to send your deposit to this address.`,
