98ee66d40094c2f067220c965df1654669e675a0
davegahn <anxieter@yandex.ru>
2019-11-27 20:32:10 +0300

    [65]Added pwd and ssl connection

--- src/connectDB.js
+++ src/connectDB.js
@@ -2,12 +2,16 @@ import mongoose from 'mongoose';
 
 const options = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 };
+
+let optionss = {};
+const SSL = true;
+optionss.SSL = SSL;
 
 export async function connectDatabase(host, port, database) {
   try {
-    await mongoose.connect(`mongodb://${host}:${port}/${database}`, options);
+    await mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${host}:${port}/${database}`, options);
 
     console.log(`Database connection successful on ${host}:${port}/${database}`);
   } catch (error) {
