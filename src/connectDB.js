f1c74a36351507f6b5b110bb931e4c241c86f70e
davegahn <anxieter@yandex.ru>
2019-11-28 13:36:06 +0300

    [66]Remove conflict

--- src/connectDB.js
+++ src/connectDB.js
@@ -8,10 +8,6 @@ const options = {
   }
 };
 
-let optionss = {};
-const SSL = true;
-optionss.SSL = SSL;
-
 export async function connectDatabase(host, port, database) {
   try {
     await mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${host}:${port}/${database}`, options);
