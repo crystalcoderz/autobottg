16edaa1ececa6e2a2544cdad8cf8e9e1a552bc05
davegahn <anxieter@yandex.ru>
2019-12-05 12:55:31 +0300

    [75]Removed packges, added limit to requests

--- src/bot.js
+++ src/bot.js
@@ -14,6 +14,7 @@ import { messages } from './messages';
 import { getMainKeyboard } from './keyboards';
 import { cancelTradeAction, handleStartAction } from './actions';
 import { config } from './config';
+import rateLimit from 'telegraf-ratelimit';
 
 const bot = new Telegraf(process.env.API_BOT_KEY);
 
@@ -36,6 +37,12 @@ const session = new RedisSession({
   }
 });
 
+const limitConfig = {
+  window: 1500,
+  limit: 1
+}
+
+bot.use(rateLimit(limitConfig));
 bot.use(session);
 bot.use(stage.middleware());
 bot.start(ctx => ctx.reply(messages.startMsg, getMainKeyboard(ctx)));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import morgan from 'morgan';
 import 'dotenv/config';
 import { connectDatabase } from './connectDB';
 import routes from './routes';
 
 const app = express();
 
-app.use(morgan('combined'));
 app.use('/', routes);
 
 app.listen(process.env.APP_PORT, async () => {
