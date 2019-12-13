39477d755325eba291a453ebacfa6c3775b827d0
anderpo <potapandre@gmail.com>
2019-12-13 17:03:29 +0300

    fix mongo warning

--- src/connectDB.js
+++ src/connectDB.js
@@ -9,6 +9,7 @@ const options = {
 export async function connectDatabase(host, port, database) {
   try {
     await mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${host}:${port}/${database}`, options);
+    mongoose.set('useFindAndModify', false);
 
     console.log(`Database connection successful on ${host}:${port}/${database}`);
   } catch (error) {
