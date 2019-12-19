7db2f9f7d0b6a11e1dc03b9fa4003bd8a3fce470
anderpo <potapandre@gmail.com>
2019-12-19 18:45:15 +0300

    fixed username not required

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   userId: { type: Number, required: true },
-  username: { type: String, required: true },
+  username: { type: String },
   visits: [{ type: Schema.Types.ObjectId, ref: 'Visit' }],
   transactions: [{ type: Schema.Types.ObjectId, ref: 'Transaction' }]
 });
