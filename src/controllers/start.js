06a446fe1f864ccc0c2cc870c7a43ef7e847adb8
anderpo <potapandre@gmail.com>
2019-12-27 17:18:11 +0300

    return disable web page preview

--- src/controllers/start.js
+++ src/controllers/start.js
@@ -7,7 +7,13 @@ const start = new Scene(scenes.start);
 start.enter(async ctx => {
   const { userId } = ctx.session;
 
-  await ctx.replyWithHTML(`Please follow this <a href="${process.env.APP_HOST_PORT}/user-ip/${userId}">link</a> to read our Terms of Use and Privacy Policy. Then, return to the bot to proceed.`, getEmptyKeyboard());
+  const opts = {
+    parse_mode: 'HTML',
+    disable_web_page_preview: true,
+    ...getEmptyKeyboard(),
+  };
+
+  await ctx.replyWithHTML(`Please follow this <a href="${process.env.APP_HOST_PORT}/user-ip/${userId}">link</a> to read our Terms of Use and Privacy Policy. Then, return to the bot to proceed.`, opts);
 });
 
 export default start;
