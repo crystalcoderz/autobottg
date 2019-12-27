1d31f5e7816924e45599fc24fc587e46ec794ada
anderpo <potapandre@gmail.com>
2019-12-27 15:01:52 +0300

    hide keyboard

--- src/controllers/start.js
+++ src/controllers/start.js
@@ -1,17 +1,13 @@
 import Scene from 'telegraf/scenes/base';
 import scenes from '../constants/scenes';
+import { getEmptyKeyboard } from '../keyboards';
 
 const start = new Scene(scenes.start);
 
 start.enter(async ctx => {
   const { userId } = ctx.session;
 
-  const opts = {
-    parse_mode: 'HTML',
-    disable_web_page_preview: true,
-  };
-
-  await ctx.reply(`Please follow this <a href="${process.env.APP_HOST_PORT}/user-ip/${userId}">link</a> to read our Terms of Use and Privacy Policy. Then, return to the bot to proceed.`, opts);
+  await ctx.replyWithHTML(`Please follow this <a href="${process.env.APP_HOST_PORT}/user-ip/${userId}">link</a> to read our Terms of Use and Privacy Policy. Then, return to the bot to proceed.`, getEmptyKeyboard());
 });
 
 export default start;
